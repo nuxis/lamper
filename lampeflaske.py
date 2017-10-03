b26dd19d240b12a05f6db56df0323fce0117821a
Mathias Bøhn Grytemark <mathias@grytemark.no>
2017-10-04 01:52:57 +0200

    add more untested changes for slack integration

--- lampeflaske.py
+++ lampeflaske.py
@@ -11,13 +11,20 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
 def hello():
-    # FIXME args/form
+
+    pprint.pprint(request.form)
+
+    if request.form.get('command') != '/lamper':
+        return "wrong command" , status.HTTP_400_BAD_REQUEST
+
+    if request.form.get('team_id') != os.environ['SLACK_TEAMID']:
+        return "wrong team id" , status.HTTP_403_FORBIDDEN
+
     if request.form.get('token') != os.environ['SLACK_TOKEN']:
         return "wrong token", status.HTTP_403_FORBIDDEN
 
-    # FIXME args/form
-    if request.form.get('channel_name') != os.environ['SLACK_CHANNEL']:
-        return "wrong channel", status.HTTP_403_FORBIDDEN
+    if request.form.get('channel_id') != os.environ['SLACK_CHANNELID']:
+        return "wrong channel id", status.HTTP_403_FORBIDDEN
 
     if request.form.get('text') not in lamper.colors.keys():
         return "wrong color" , status.HTTP_400_BAD_REQUEST
